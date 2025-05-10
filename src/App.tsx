81e896855f8112be287b65e5d7758a1e2b185b51
iamsameeraliyanage <iamsameeraliyanage@gmail.com>
2025-05-10 21:23:13 +0530

    Add Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { AppRoutes } from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "./theme/ThemeProvider";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient();
 
